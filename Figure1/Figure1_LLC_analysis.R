f857492346ba2ae069c313a3d23149c189ce58fc
s-shichino1989 <86090391+s-shichino1989@users.noreply.github.com>
2021-06-22 20:31:59 +0900

    Add files via upload

---
+++ Figure1/Figure1_LLC_analysis.R
@@ -0,0 +1,65 @@
+#re-analysis of murine Lewis Lung carcinoma TAS-Seq data
+options(stringsAsFactors = FALSE)
+suppressWarnings(suppressMessages(source("/datadrive/Rhapsody_analysis/Rscripts/library_source_Seurat.R")))
+
+tableread_fast_hashtag = function(x, sep="\t", header=TRUE){
+  tmp = data.table::fread(x, header=header, sep=sep, quote="")
+  tmp = as.data.frame(tmp)
+  return(tmp)
+}
+
+#load gene expression matrix
+tablelist = lapply("matrix_inflection_LLC.txt.gz", 
+                   tableread_fast_sparse)
+names(tablelist)="LLC"
+
+#load hashtag-expression matrix
+hashtag_data=lapply("Hashtag_top1M_LLCTag1.txt.gz", tableread_fast_hashtag)
+hashtag_data[[1]][,1] = paste(names(tablelist)[1], hashtag_data[[1]][,1], sep='_')
+quiet(gc())
+
+##create Seurat object (Seurat v2 workflow) and annotate by hashtag
+colnames(tablelist[[1]]) = paste(names(tablelist)[1], colnames(tablelist[[1]]), sep='_')
+seu = lapply(tablelist, CreateSeuratObject, min.cells = 5, min.genes = 500)
+
+#Demultiplexing and generate centered log heatmap (Figure 1f)
+seu = Demultiplex_DNAtags(hashtag_data[[1]], seu[[1]],
+                          scale.factor = max(colSums(hashtag_data[[1]][,2:ncol(hashtag_data[[1]])])),
+                          nTags=14, nCells_in_cartridge=nrow(seu[[1]]@meta.data), 
+                          sample.name="LLC",
+                          dir.name="./")
+
+quiet(file.remove("LLC_demultiplex.txt"))
+
+#export the number of cells which were assigned toeach Hashtag
+tmp = table(seu[[1]]@meta.data$TagIDs)
+tmp = as.data.frame(tmp)
+rownames(tmp)=tmp[,1]
+logs_Ncells = as.data.frame(t(tmp))
+logs_Ncells = logs_Ncells[2,,drop=F]
+rownames(logs_Ncells)="cell number"
+write.table(logs_Ncells, "cellnumber.txt", sep="\t", quote=F)
+
+#re-generate Seurat object for demultiplexed sample
+raw.data = seu[[1]]@raw.data
+tmp = paste(seu[[1]]@meta.data$TagIDs, rownames(seu[[1]]@meta.data), sep="_")
+colnames(raw.data)=as.character(tmp)
+colnames(raw.data)=gsub("not_detected", "not-detected", colnames(raw.data))
+seu=NULL
+seu = CreateSeuratObject(raw.data, min.cells=5, min.genes=500)
+colnames(seu@meta.data)[2]="nReads"
+save.pigz(seu, file="LLC_demultiplex.rda", n.cores=16)
+
+#Ridgeline plot (Figure 1f)
+p = RidgePlot(seu, features.plot="nGene", size.title.use = 0)
+p = p + theme(title = element_blank(), axis.title.y = element_blank())
+
+ggsave(file="genes_Ridge.png", plot=p, device="png", units="in",
+       dpi=300, width=3.5, height=3.5, limitsize = FALSE)
+
+
+
+
+
+
+
