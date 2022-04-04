4c221659fdf55bd2d3f7144b5e437cd9a7f02578
s-shichino1989 <86090391+s-shichino1989@users.noreply.github.com>
2022-04-04 13:03:46 +0900

    Update Figure4a_4b.R

--- Figure4/Figure4a_4b.R
+++ Figure4/Figure4a_4b.R
@@ -32,6 +32,9 @@ custom_colors$discrete <- unique(c(colors_dutch, colors_spanish, colors_custom))
 
 
 ##download annotated Seurat objects for mouse lung data
+system("wget -L -O matrix_inflection_10Xv2-GSM3926540.txt.gz 
+       https://tus.box.com/shared/static/wf4whgfdj2bx0kpbe05r66ntq1p7zltl.gz")
+
 system("wget -L -O UTlung_deep_Seurat_annot.qs
        https://tus.box.com/shared/static/ohoohhgf3t12tcmupueajbbapu4xdxm6.qs")
 system("wget -L -O UTlung_shallow_Seurat_annot.qs
