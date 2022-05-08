3e0835ec685d234e27e74e0b1b92701f27989906
s-shichino1989 <86090391+s-shichino1989@users.noreply.github.com>
2022-05-08 20:38:05 +0900

    Add files via upload

---
+++ preprocessing/TASSeq.dataset2/DC_subset_analysis.R
@@ -0,0 +1,69 @@
+##TAS-Seq lung dataset2 subsetting
+
+quiet <- function(x) { 
+  sink(tempfile()) 
+  on.exit(sink()) 
+  invisible(force(x)) 
+} 
+
+
+mBC = qread("./Seurat/day00UT_results_Seurat.qs")
+mBC1 = mBC
+
+#DC
+
+hoge = mBC1@assays$RNA@counts[,rownames(mBC1@meta.data[mBC1@meta.data$RNA_snn_res.3.5 %in% c(22),])]
+mBC = CreateSeuratObject(hoge)
+mBC = NormalizeData(mBC, scale.factor=1000000)
+mBC = FindVariableFeatures(mBC, selection.method = "mvp", 
+                           mean.cutoff=c(0.1, Inf), dispersion.cutoff=c(0.5, Inf))
+message("Scaling data...")
+
+colnames(mBC@meta.data) = c("orig.ident", "nReads", "nGene")
+mBC = ScaleData(object = mBC, vars.to.regress = c("nReads"))
+hvg.number = length(x = mBC@assays$RNA@var.features)
+mBC = RunPCA(object = mBC, features = VariableFeatures(object = mBC), 
+             npcs=50, verbose = FALSE)
+
+
+mBC = JackStraw(mBC, num.replicate = 100, dims = 50, verbose = FALSE)
+mBC = ScoreJackStraw(mBC, dims = 1:50, score.thresh = 0.05)
+#return p-value of Jackstraw analysis
+JS_res = mBC@reductions$pca@jackstraw$overall.p.values
+tmp = JS_res[JS_res[,2]> 1e-05,,drop=F]
+dims =c(1:(min(tmp[,1])-1))
+
+
+if(nrow(mBC@meta.data)/200 >=500){
+  perplexity = 500
+} else if (nrow(mBC@meta.data)/200 >=300){
+  perplexity = 400 
+} else if (nrow(mBC@meta.data)/200 >=200){
+  perplexity = 300 
+} else if (nrow(mBC@meta.data)/200 >=100){
+  perplexity = 200 
+} else if (nrow(mBC@meta.data)/200 >=50){
+  perplexity = 100   
+}else if (nrow(mBC@meta.data)/200 >=25){
+  perplexity = 50   
+}else if (nrow(mBC@meta.data)/200 >=12.5){
+  perplexity = 25   
+}else {
+  perplexity = 10 
+}
+
+#perform FItSNE dimReduction
+quiet(py_set_seed(42))
+mBC = DoFItSNE(mBC, reduction_use = "pca", dims_use = as.integer(dims),
+               perplexity=perplexity, n_jobs=as.integer(detectCores()/2), df = 0.9)
+quiet(py_set_seed(42))
+mBC = RunUMAP(mBC, reduction = "pca", dims = dims)
+
+mBC = FindNeighbors(mBC, dims=dims)
+mBC = FindClusters(object = mBC, resolution = 1.0, random.seed=42, verbose = FALSE)
+DimPlot(mBC, reduction = "umap", pt.size=1, label = TRUE)
+FeaturePlot(mBC, features=c("Xcr1", "Cd209a", "Siglech", "Fcgr1", "Cx3cr1", "Ccr7"), reduction="umap")
+
+write.table(table(mBC@active.ident, mBC@meta.data$orig.ident), file="cDC1_cDC2_gate1_Cellnumber.txt", sep="\t", quote=F, row.names=T, col.names=T)
+save(mBC, file="cDC1_cDC2_gate.rda")
+
---
+++ preprocessing/TASSeq.dataset2/ident_rename.R
@@ -0,0 +1,71 @@
+
+library(Seurat)
+library(dplyr)
+library(rDBEC)
+library(qs)
+
+#rename idents of deep sequencing data
+
+#rename DC_IM object
+load("./cDC1_cDC2_gate.rda")
+new.ident = c("DC_cDC1", "DC_cDC2")
+names(new.ident)=levels(mBC@active.ident)
+
+mBC = RenameIdents(mBC, new.ident)
+
+hoge = mBC@active.ident
+hoge = as.character(hoge)
+names(hoge)=rownames(mBC@meta.data)
+
+mBC = AddMetaData(mBC, metadata = hoge, col.name="celltype")
+
+DC_Metadata = mBC@meta.data
+
+#rename entire data
+
+mBC = qread("./Seurat/day00UT_results_Seurat.qs", nthreads=12)
+
+new.ident = read.table("./TASSeq_dataset2_annot.txt", row.names=1, header = T, sep="\t", quote="")
+
+tmp = new.ident[,1]
+names(tmp)=as.numeric(rownames(new.ident))
+
+mBC = RenameIdents(mBC, tmp)
+
+hoge = mBC@active.ident
+hoge = as.character(hoge)
+names(hoge)=rownames(mBC@meta.data)
+
+mBC = AddMetaData(mBC, metadata = hoge, col.name="celltype")
+
+#add DC/IM annotation
+hoge = mBC@meta.data
+
+hoge = hoge[!hoge$RNA_snn_res.3.5 %in% "22",]
+hoge = hoge[,c(1,24)]
+hoge1 = DC_Metadata[,c(1,9)]
+
+hoge2 = rbind(hoge, hoge1)
+
+hoge3 = hoge2$celltype
+names(hoge3)=rownames(hoge2)
+mBC = AddMetaData(mBC, metadata = hoge3, col.name="celltype")
+
+#change active.ident
+hoge3 = factor(hoge3, levels = sort(unique(hoge3)))
+mBC@active.ident = hoge3
+
+#save renamed object
+qsave(mBC, file="./Seurat/UTlung_TASSeq_dataset2_Seurat_annot.qs", nthreads=12)
+
+#export cell subset number
+tmp = table(mBC@meta.data$celltype, mBC@meta.data$orig.ident)
+tmp = as.data.frame(tmp)
+tmp1 = tmp %>% tidyr::spread(key = Var2, value=Freq) 
+colnames(tmp1)[1]="celltype"
+hoge = c("doublet", "not-detected")
+tmp1 = tmp1[,!colnames(tmp1) %in% hoge]
+
+write.table(tmp1, "./Seurat/Seurat_tables/UTlung_TASSeq_dataset2_results_subsetNumber_annot.txt", row.names=F, sep="\t", quote=F)
+
+
